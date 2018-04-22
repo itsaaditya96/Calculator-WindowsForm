f69f295a471ebd2e9c5de34c33ca53694337fb18
Addy <addyrockx96@gmail.com>
2018-04-22 18:35:37 +0530

    Imported

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) / Convert.ToInt32(textBox2.Text));
+        }
+
+         }
+}
